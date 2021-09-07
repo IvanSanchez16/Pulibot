36601eecdbef63192aba0612adb912cdbfdf5a8e
Ivan Sanchez <ivanhumberto_16@hotmail.com>
2021-09-06 20:08:24 -0600

    Olvide stagear este archivo :(

---
+++ Models/google_api.py
@@ -0,0 +1,12 @@
+from googleapiclient.discovery import build
+from decouple import config
+
+KEY = config('GOOGLE_KEY')
+
+
+def get_URL(title):
+    with build('youtube', 'v3', developerKey=KEY) as service:
+        request = service.search().list(maxResults=1, regionCode='mx', part='snippet', q=title, type='video')
+        response = request.execute()
+        videoID = response['items'][0]['id']['videoId']
+        return f'https://youtube.com/watch?v={videoID}'
