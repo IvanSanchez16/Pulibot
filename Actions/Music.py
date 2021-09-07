9c46721d911b0274402568878b17a1a66f2a70f4
Ivan Sanchez <ivanhumberto_16@hotmail.com>
2021-09-06 22:04:48 -0600

    Cola de reproducción y canciones con más de una palabra

---
+++ Actions/Music.py
@@ -0,0 +1,40 @@
+from Actions.Message import send_embed
+from Models.ytdl import YTDLSource
+import asyncio
+
+queue = []
+
+
+async def add_queue(voice_client, url, client):
+    song = await YTDLSource.from_url(url)
+    title = song['title']
+    field = f'[{title}]({url})'
+
+    if len(queue) == 0:
+        song['url'] = url
+        queue.append(song)
+        voice_client.play(song['player'], after=lambda e: next_song(e, client, voice_client))
+        await send_embed(channel='comandos', duration_on_secs=song['duration'], title='Reproduciendo',
+                         image=song['image'], color=(226, 12, 12), client=client, description=field)
+        return
+    song['url'] = url
+    queue.append(song)
+    await send_embed(channel='comandos', title=f'Añadido a la cola - Posición: {len(queue)-1}',
+                     image=song['image'], color=(226, 12, 12), client=client, description=field)
+
+
+def next_song(error, client, voice_client):
+    if not error:
+        if len(queue) == 1:
+            queue.pop(0)
+            return
+        if len(queue) > 1:
+            queue.pop(0)
+            song = queue[0]
+            title = song['title']
+            url = song['url']
+            field = f'[{title}]({url})'
+            voice_client.play(song['player'], after=lambda e: next_song(e, client, voice_client))
+            asyncio.run_coroutine_threadsafe(send_embed(channel='comandos', duration_on_secs=song['duration'], title='Reproduciendo',
+                             image=song['image'], color=(226, 12, 12), client=client, description=field), client.loop)
+
--- Listeners/music_commands.py
+++ Listeners/music_commands.py
@@ -1,6 +1,6 @@
-from Actions.Message import send_message, send_embed
-from Models.ytdl import YTDLSource
+from Actions.Message import send_message
 from Models.google_api import get_URL
+from Actions.Music import add_queue
 
 
 async def play_song(ctx, params, client):
@@ -15,18 +15,20 @@ async def play_song(ctx, params, client):
         return
 
     channel = channel.channel
-    await channel.connect()
+    if len(client.voice_clients) != 0:
+        if channel != client.voice_clients[0].channel:
+            await send_message(channel='comandos', message='Ya estoy ocupado en otro canal',
+                               client=client)
+            return
+    else:
+        await channel.connect()
 
     server = ctx.message.guild
-    voice_channel = server.voice_client
-    url = get_URL(params[0])
+    voice_client = server.voice_client
 
-    async with ctx.typing():
-        song = await YTDLSource.from_url(url)
-        voice_channel.play(song['player'], after=lambda e: print('Player error: %s' % e) if e else None)
-        title = song['title']
-        field = f'[{title}]({url})'
+    title = ''
+    for p in params:
+        title = title + ' ' + p
+    url = get_URL(title)
 
-    await send_embed(channel='comandos', duration_on_secs=song['duration'], title='Reproduciendo',
-                     image=song['image'], color=(226, 12, 12), client=client, description= field)
-
+    await add_queue(voice_client, url, client)
